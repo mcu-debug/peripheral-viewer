ece818961b1c29d202cdd888a61c638945813a50
Rob Moran <rob.moran@arm.com>
2024-03-05 09:27:00 +0000

    Update pack asset service URL (#22)

--- src/manifest.ts
+++ src/manifest.ts
@@ -12,6 +12,6 @@ export const DEFAULT_PROCESSOR = 'processorName';
 export const CONFIG_ADDRGAP = 'svdAddrGapThreshold';
 export const DEFAULT_ADDRGAP = 16;
 export const CONFIG_ASSET_PATH = 'packAssetUrl';
-export const DEFAULT_ASSET_PATH = 'https://pack-asset-service.keil.arm.com';
+export const DEFAULT_ASSET_PATH = 'https://pack-content.cmsis.io';
 export const CONFIG_SAVE_LAYOUT = 'saveLayout';
 export const DEBUG_LEVEL = 'debugLevel';
